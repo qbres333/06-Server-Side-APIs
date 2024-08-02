c18a842214f67c1fc67aaf37858a1e5f3234b39e
Fin M <qbres333@hotmail.com>
2024-08-01 22:34:43 -0600

    class activities

--- 01-Activities/24-Stu_Review-Part-Two/Unsolved/assets/js/single.js
+++ 01-Activities/24-Stu_Review-Part-Two/Unsolved/assets/js/single.js
@@ -1,13 +1,13 @@
-const repoNameEl = document.querySelector('#repo-name');
-const issueContainerEl = document.querySelector('#issues-container');
-const limitWarningEl = document.querySelector('#limit-warning');
+const repoNameEl = document.querySelector("#repo-name");
+const issueContainerEl = document.querySelector("#issues-container");
+const limitWarningEl = document.querySelector("#limit-warning");
 
 const getRepoName = function () {
   // Where is this value coming from?
   // TODO: Write your answer here
   /** the parameter section of the URL, after the question mark */
   const queryString = document.location.search;
-  const repoName = queryString.split('=')[1];
+  const repoName = queryString.split("=")[1];
 
   if (repoName) {
     repoNameEl.textContent = repoName;
@@ -17,7 +17,7 @@ const getRepoName = function () {
     // Under what condition will this run?
     // TODO: Write your answer here
     /** when there's no repoName in the query */
-    document.location.replace('./index.html');
+    document.location.replace("./index.html");
   }
 };
 
@@ -31,14 +31,14 @@ const getRepoIssues = function (repo) {
 
         // What is this checking for? Under what condition will this be `true`?
         // TODO: Write your answer here
-        /** if the returned data has a link in the header (i.e. returned data 
+        /** if the returned data has a link in the header (i.e. returned data
          * has a value), display warning (more than 30 issues) */
-        if (response.headers.get('Link')) {          
+        if (response.headers.get("Link")) {
           displayWarning(repo);
         }
       });
     } else {
-      document.location.replace('./index.html');
+      document.location.replace("./index.html");
     }
   });
 };
@@ -47,26 +47,26 @@ const displayIssues = function (issues) {
   // Is there a difference between this and `!issues.length`?
   // TODO: Write your answer here
   if (issues.length === 0) {
-    issueContainerEl.textContent = 'This repo has no open issues!';
+    issueContainerEl.textContent = "This repo has no open issues!";
     return;
   }
 
   for (let issueObj of issues) {
-    const issueEl = document.createElement('a');
-    issueEl.classList = 'list-item flex-row justify-space-between align-center';
-    issueEl.setAttribute('href', issueObj.html_url);
-    issueEl.setAttribute('target', '_blank');
+    const issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issueObj.html_url);
+    issueEl.setAttribute("target", "_blank");
 
-    const titleEl = document.createElement('span');
+    const titleEl = document.createElement("span");
     titleEl.textContent = issueObj.title;
     issueEl.appendChild(titleEl);
 
-    const typeEl = document.createElement('span');
+    const typeEl = document.createElement("span");
 
     if (issueObj.pull_request) {
-      typeEl.textContent = '(Pull request)';
+      typeEl.textContent = "(Pull request)";
     } else {
-      typeEl.textContent = '(Issue)';
+      typeEl.textContent = "(Issue)";
     }
 
     issueEl.appendChild(typeEl);
@@ -78,12 +78,12 @@ const displayIssues = function (issues) {
 // What does this function do?
 // TODO: Write your answer here
 const displayWarning = function (repo) {
-  limitWarningEl.textContent = 'To see more than 30 issues, visit ';
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
-  const linkEl = document.createElement('a');
-  linkEl.textContent = 'GitHub.com';
-  linkEl.setAttribute('href', `https://github.com/${repo}/issues`);
-  linkEl.setAttribute('target', '_blank');
+  const linkEl = document.createElement("a");
+  linkEl.textContent = "GitHub.com";
+  linkEl.setAttribute("href", `https://github.com/${repo}/issues`);
+  linkEl.setAttribute("target", "_blank");
 
   // Where does this appear on the page?
   // TODO: Write your answer here
